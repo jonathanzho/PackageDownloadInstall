02476b2d32f537eb50496592a6d299da92293d3f
Jonathan Zhong <33461789+jonathanzho@users.noreply.github.com>
2018-06-15 10:58:43 -0700

    Fixed most compiler-issued warnings.

--- app/src/main/java/com/example/jonathanzhong/packagedownloadinstall/MainActivity.java
+++ app/src/main/java/com/example/jonathanzhong/packagedownloadinstall/MainActivity.java
@@ -3,23 +3,19 @@ package com.example.jonathanzhong.packagedownloadinstall;
 import android.Manifest;
 import android.app.Activity;
 import android.app.DownloadManager;
-import android.app.PendingIntent;
-import android.app.ProgressDialog;
 import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.IntentSender;
-import android.content.pm.PackageInstaller;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -33,30 +29,15 @@ import android.view.View;
 import android.webkit.MimeTypeMap;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
   private static final String TAG = MainActivity.class.getSimpleName();
 
   private static final String APK_PACKAGE_NAME = "com.example.jonathan.payjoypackagemonitor";
   private static final String APK_FILE_NAME = "PayJoyPackageMonitor.apk";
-
   private static final String APK_DOWNLOAD_URL = "https://github.com/jonathanzho/resFiles/raw/master/apk/" + APK_FILE_NAME;
 
-  private static final String ACTION_INSTALL_COMPLETE = "com.example.jonathanzhong.packagedownloadinstall.INSTALL_COMPLETE";
-
-  ProgressDialog pd;
-
-  private long m_downloadId = 0;
   private DownloadManager m_downloadManager;
-  private File m_downloadDir;
-  private String m_downloadedApkFilePath;
-  private Uri m_downloadedApkUri;
   private BroadcastReceiver m_downloadCompleteReceiver;
 
   @Override
@@ -65,10 +46,10 @@ public class MainActivity extends AppCompatActivity {
 
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    Toolbar toolbar = findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    FloatingActionButton fab = findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
@@ -79,12 +60,6 @@ public class MainActivity extends AppCompatActivity {
 
     // Customization starts from here.
 
-    // Get runtime storage permission for API 23 and up:
-    //isStoragePermissionGranted();
-
-    m_downloadDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-    m_downloadedApkFilePath = m_downloadDir + "/" + APK_FILE_NAME;    // ???
-    m_downloadedApkUri = Uri.parse("file://" + m_downloadedApkFilePath);
     m_downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
 
     m_downloadCompleteReceiver = new BroadcastReceiver() {
@@ -99,9 +74,11 @@ public class MainActivity extends AppCompatActivity {
       }
     };
 
-    isStoragePermissionGranted();
+    boolean storageGranted = checkStoragePermissionBeforeDownloading();
 
-    Log.d(TAG, "onCreate: end");
+    Log.v(TAG, "onCreate: storageGranted=[" + storageGranted + "]");
+
+    Log.v(TAG, "onCreate: end");
   }
 
   @Override
@@ -140,7 +117,7 @@ public class MainActivity extends AppCompatActivity {
         request.setMimeType(getMimeType(uri.toString()));
         request.setTitle(fileName);
         request.setDescription("Downloading attachment...");
-        request.allowScanningByMediaScanner();;
+        request.allowScanningByMediaScanner();
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
         request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
         m_downloadManager.enqueue(request);
@@ -205,61 +182,24 @@ public class MainActivity extends AppCompatActivity {
     }
   }
 
-  public static boolean installPackage(Context context, InputStream inputStream, String packageName) throws IOException {
-    Log.d(TAG, "installPackage: start");
-
-    PackageInstaller packageInstaller = context.getPackageManager().getPackageInstaller();
-    PackageInstaller.SessionParams params = new PackageInstaller.SessionParams(
-        PackageInstaller.SessionParams.MODE_FULL_INSTALL);
-    params.setAppPackageName(packageName);
-    int sessionId = packageInstaller.createSession(params);
-    PackageInstaller.Session session = packageInstaller.openSession(sessionId);
-    OutputStream outputStream = session.openWrite("COSU", 0, -1);
-    byte[] buffer = new byte[65536];
-    int count;
-    while ((count = inputStream.read(buffer)) != -1) {
-      outputStream.write(buffer, 0, count);
-    }
-    session.fsync(outputStream);
-    inputStream.close();
-    outputStream.close();
-
-    session.commit(createIntentSender(context, sessionId));
-
-    Log.d(TAG, "installPackage: end");
-
-    return true;
-  }
-
-  private static IntentSender createIntentSender(Context context, int sessionId) {
-    PendingIntent pendingIntent = PendingIntent.getBroadcast(
-        context,
-        sessionId,
-        new Intent(ACTION_INSTALL_COMPLETE),
-        0);
-
-    return pendingIntent.getIntentSender();
-  }
-
-
-  public boolean isStoragePermissionGranted() {
+  public boolean checkStoragePermissionBeforeDownloading() {
     if (Build.VERSION.SDK_INT >= 23) {
       if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
           == PackageManager.PERMISSION_GRANTED) {
-        Log.v(TAG, "isStoragePermissionGranted: API 23 and up: YES");
+        Log.v(TAG, "checkStoragePermissionBeforeDownloading: API 23 and up: YES");
 
         downloadFile(this, m_downloadCompleteReceiver, APK_DOWNLOAD_URL, APK_FILE_NAME);
 
         return true;
       } else {
-        Log.v(TAG, "isStoragePermissionGranted: API 23 and up: NO. requesting...");
+        Log.v(TAG, "checkStoragePermissionBeforeDownloading: API 23 and up: NO. requesting...");
 
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
 
         return false;
       }
     } else {
-      Log.v(TAG, "isStoragePermissionGranted: API 22 and down: YES");
+      Log.v(TAG, "checkStoragePermissionBeforeDownloading: API 22 and down: YES");
 
       downloadFile(this, m_downloadCompleteReceiver, APK_DOWNLOAD_URL, APK_FILE_NAME);
 
@@ -268,107 +208,15 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @Override
-  public void onRequestPermissionsResult(int requestCode, String[] permissions, int grantResults[]) {
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int grantResults[]) {
     super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
     if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
       Log.v(TAG, "onRequestPermissionsResult: Storage permission GRANTED");
 
-      // Start downloading and installing:
-      //DownloadInstallPackageTask dipTask = new DownloadInstallPackageTask();
-      //dipTask.execute(APK_DOWNLOAD_URL);
-
       downloadFile(this, m_downloadCompleteReceiver, APK_DOWNLOAD_URL, APK_FILE_NAME);
     } else {
       Log.v(TAG, "onRequestPermissionsResult: Storage permission DENIED");
     }
-  }
-
-  private class DownloadInstallPackageTask extends AsyncTask<String, String, String> {
-    private final String TAG2 = DownloadInstallPackageTask.class.getSimpleName();
-
-    int styatus = 0;
-
-    protected void onPreExecute() {
-      Log.d(TAG2, "onPreExecute");
-
-      super.onPreExecute();
-
-      pd = new ProgressDialog(MainActivity.this);
-      pd.setMessage("Please wait!");
-      pd.setCancelable(false);
-      pd.show();
-    }
-
-    @Override
-    protected String doInBackground(String... params) {
-      Log.d(TAG2, "doInBackground");
-
-      HttpURLConnection connection;
-
-      try {
-        URL url = new URL(params[0]);
-        connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.setDoOutput(true);
-        connection.connect();
-
-        File folder = new File(Environment.getExternalStorageDirectory() + "/temp");
-        boolean success = true;
-        if (!folder.exists()) {
-          success = folder.mkdir();
-        }
-        if (!success) {
-          Log.e(TAG2, "doInBackground: failed making folder temp on external storage");
-          return null;
-        }
-
-        File outputFile = new File(folder, "temp.apk");
-        if (outputFile.exists()) {
-          outputFile.delete();
-          Log.v(TAG2, "doInBackground: deleted existing temp.apk");
-        }
-
-        FileOutputStream fos = new FileOutputStream(outputFile);
-
-        InputStream is = connection.getInputStream();
-
-        byte[] buffer = new byte[1024];
-        int len1 = 0;
-
-        while ((len1 = is.read(buffer)) != -1) {
-          fos.write(buffer, 0, len1);
-        }
-
-        fos.flush();
-        fos.close();
-        is.close();
-
-        Log.v(TAG2, "doInBackground: downloaded temp.apk");
-
-        // Install
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setDataAndType(Uri.fromFile(outputFile), "application/vnd.android.package-archive");
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        getApplicationContext().startActivity(intent);
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-
-      return null;
-    }
-
-    @Override
-    protected void onPostExecute(String result) {
-      Log.d(TAG2, "onPostExecute: result=[" + result + "]");
-
-      super.onPostExecute(result);
-
-      // Dismiss progress dialog
-      if (pd.isShowing()) {
-        pd.dismiss();
-      }
-    }
-
   }
 }
