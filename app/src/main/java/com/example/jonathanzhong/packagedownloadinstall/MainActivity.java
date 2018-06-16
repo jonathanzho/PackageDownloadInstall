aa0290dfa60ef66a2e3a497de0111ebb3200d63f
Jonathan Zhong <33461789+jonathanzho@users.noreply.github.com>
2018-06-16 01:27:52 -0700

    No APK downloading if package already installed.

--- app/src/main/java/com/example/jonathanzhong/packagedownloadinstall/MainActivity.java
+++ app/src/main/java/com/example/jonathanzhong/packagedownloadinstall/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.jonathanzhong.packagedownloadinstall;
 
 import android.Manifest;
-import android.app.Activity;
 import android.app.DownloadManager;
 import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
@@ -9,6 +8,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
   private DownloadManager m_downloadManager;
   private BroadcastReceiver m_downloadCompleteReceiver;
+  private boolean m_apkInstalled;
+  private boolean m_apkDownloaded;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -74,6 +76,9 @@ public class MainActivity extends AppCompatActivity {
       }
     };
 
+    m_apkInstalled = isPackageInstalled(APK_PACKAGE_NAME);
+    m_apkDownloaded = false;
+
     boolean storageGranted = checkStoragePermissionBeforeDownloading();
 
     Log.v(TAG, "onCreate: storageGranted=[" + storageGranted + "]");
@@ -106,13 +111,18 @@ public class MainActivity extends AppCompatActivity {
   // New methods
   // ===========
 
-  public void downloadFile(final Activity activity, final BroadcastReceiver receiver, final String url, final String fileName) {
+  public void downloadFile(final Context context, final BroadcastReceiver receiver, final String url, final String fileName) {
     Log.d(TAG, "downloadFile");
+
+    if (m_apkInstalled) {
+      Log.i(TAG, "downloadFile: APK=[" + APK_PACKAGE_NAME + "] is already installed. No downloading.");
+      return;
+    }
 
     try {
       if (url != null && !url.isEmpty()) {
         Uri uri = Uri.parse(url);
-        activity.registerReceiver(receiver, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+        context.registerReceiver(receiver, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
         DownloadManager.Request request = new DownloadManager.Request(uri);
         request.setMimeType(getMimeType(uri.toString()));
         request.setTitle(fileName);
@@ -125,6 +135,17 @@ public class MainActivity extends AppCompatActivity {
     } catch (IllegalStateException e) {
       e.printStackTrace();
     }
+  }
+
+  public boolean isPackageInstalled(String targetPackage) {
+    PackageManager pm = getPackageManager();
+    try {
+      PackageInfo info = pm.getPackageInfo(targetPackage, PackageManager.GET_META_DATA);
+    } catch (PackageManager.NameNotFoundException e) {
+      e.printStackTrace();
+      return false;
+    }
+    return true;
   }
 
   private String getMimeType(String url) {
